b4b3e84d77e6512388e1b4b29f6334f5c0796a48
loxiz <adrian.dahlman@studerande.karlskogafhs.se>
2022-01-25 10:05:12 +0100

    mass uppdate

---
+++ src/js/cart.js
@@ -0,0 +1,12 @@
+const cart = () => {
+
+    const addButton = document.querySelector('.sidebar__button')
+    const counter = document.querySelector('.header__counter')
+    const lessButton = document.querySelector('.product__less')
+    const moreButton = document.querySelector('.product__more')
+    const input = document.querySelector('.product__form')
+
+
+}
+
+cart()
